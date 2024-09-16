19d141318e8b2768cb623aa040ec78a9e936e3ab
prismtechbd3 <prismtechb@gmail.com>
2024-09-16 18:13:39 +0600

    continue

--- stores/auth.js
+++ stores/auth.js
@@ -1,3 +1,5 @@
+import { useTokenStore } from "./token";
+
 export const useAuthStore = defineStore('auth', {
     state: () => ({ count: 0, name: 'Eduardo' }),
 
@@ -7,12 +9,14 @@ export const useAuthStore = defineStore('auth', {
     
     actions: {
          async login(formData){
+          const token = useTokenStore();
             try {
               const {data} = await $fetch("http://127.0.0.1:8000/api/login", {
                 method: "POST",
                 body: { ...formData },
               });
               console.log("auth_store", data);
+              token.setToken(data.token);
             } catch (error) {
               throw error
             }
